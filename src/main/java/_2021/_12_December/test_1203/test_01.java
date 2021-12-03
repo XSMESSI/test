b197161db60f00f5b780cd13c4eedfff4468eb31
xushen <shen.xu@campsg.cn>
2021-12-03 15:06:33 +0800

    测试提交

---
+++ src/main/java/_2021/_12_December/test_1203/test_01.java
@@ -0,0 +1,14 @@
+package _2021._12_December.test_1203;
+
+/**
+ * @Description
+ * @Author XuShen
+ * @Date 2021/12/3 15:05
+ */
+public class test_01 {
+    public static void main(String[] args) {
+        /**
+         * 测试代码
+         */
+    }
+}
