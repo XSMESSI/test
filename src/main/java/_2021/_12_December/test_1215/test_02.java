bb37e964bb9f48e84e446538f7641cfd3387772e
xushen <shen.xu@campsg.cn>
2021-12-23 15:30:13 +0800

    差集处理

--- src/main/java/_2021/_12_December/test_1215/test_02.java
+++ src/main/java/_2021/_12_December/test_1215/test_02.java
@@ -3,19 +3,21 @@ package _2021._12_December.test_1215;
 import java.util.*;
 
 /**
- * @Description 差集计算，list转为set集合
+ * @Description 差集计算
  * @Author XuShen
  * @Date 2021/12/20 18:39
  */
 public class test_02 {
     public static void main(String[] args) {
+        //差集的含义：bto有的元素，但是view没有的元素
         List<String> listbto = Arrays.asList(new String[]{"1","2","3","5","6","7"});
-        List<String> listView = Arrays.asList(new String[]{"2","3","4"});
+        List<String> listView = Arrays.asList(new String[]{"2","3","4","8"});
         Set<String> setBto = new HashSet<>(listbto);
         Set<String> setView = new HashSet<>(listView);
         setBto.removeAll(setView);
-        System.out.println(setBto);
-
+//        setView.removeAll(setBto);
+        System.out.println("setBto = " + setBto);
+//        System.out.println("setView = " + setView);
     }
 
 }
