6db0990e937ff0fffed777ac99dd4f7282bb6148
xushen <shen.xu@campsg.cn>
2022-05-23 21:33:03 +0800

    计算代码运行时间计算

--- src/main/java/_2022/_05_May/test_0523/test_01.java
+++ src/main/java/_2022/_05_May/test_0523/test_01.java
@@ -1,5 +1,9 @@
 package _2022._05_May.test_0523;
 
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +14,7 @@ import java.util.List;
  */
 public class test_01 {
     public static void main(String[] args) {
+        LocalDateTime startTime = LocalDateTime.now();
         List<Integer> list = new ArrayList<>();
         for (int i = 1;i <= 475;i++){
             list.add(i);
@@ -27,5 +32,23 @@ public class test_01 {
             System.out.println("i = " + i);
             System.out.println("data = " + data);
         }
+
+        LocalDateTime endTime = LocalDateTime.now();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //获取当前系统时间戳
+        Duration duration = Duration.between(startTime,endTime);
+        long days = duration.toDays();//相差的天数
+        System.out.println("days = " + days);
+        long hours = duration.toHours();//相差的小时数
+        System.out.println("hours = " + hours);
+        long minutes = duration.toMinutes();//相差的分钟数
+        System.out.println("minutes = " + minutes);
+        long mills = duration.toMillis();//相差的毫秒数
+        System.out.println("mills = " + mills);
+        long nanos = duration.toNanos();//相差的纳秒数
+        System.out.println("nanos = " + nanos);
+
+
     }
 }
---
+++ src/main/java/_2022/_05_May/test_0523/test_03.java
@@ -0,0 +1,27 @@
+package _2022._05_May.test_0523;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+/**
+ * @Description 计算两个时间的相差天数，
+ * @Author XuShen
+ * @Date 2022/5/23 21:21
+ */
+public class test_03 {
+    public static void main(String[] args) {
+        LocalDateTime start = LocalDateTime.of(2020,2,13,2,23);
+        LocalDateTime end = LocalDateTime.of(2022,5,2,3,19);
+        Duration duration = Duration.between(start,end);
+        long days = duration.toDays();//相差的天数
+        System.out.println("days = " + days);
+        long hours = duration.toHours();//相差的小时数
+        System.out.println("hours = " + hours);
+        long minutes = duration.toMinutes();//相差的分钟数
+        System.out.println("minutes = " + minutes);
+        long mills = duration.toMillis();//相差的毫秒数
+        System.out.println("mills = " + mills);
+        long nanos = duration.toNanos();//相差的纳秒数
+        System.out.println("nanos = " + nanos);
+    }
+}
